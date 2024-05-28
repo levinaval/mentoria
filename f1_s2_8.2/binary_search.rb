50f63a03bf9e21832442a7012c34a89f13133713
Levi Santos <levisantoslunae@gmail.com>
2024-05-28 19:58:48 -0300

    Coreção em Binary Search.rb

--- f1_s2_8.2/binary_search.rb
+++ f1_s2_8.2/binary_search.rb
@@ -4,6 +4,25 @@ def binary_search(array, value)
 
   while inicio <= fim
     meio = (inicio + fim) / 2
+
+    if array[meio] == value
+      return meio
+     elsif array[meio] < value
+      inicio = meio + 1
+     else
+      fim = meio - 1
+    end
   end
- 
+  
+  return nil 
+end
+
+array = [10, 23, 45, 70, 80, 99]
+value_to_find = 70
+
+index = binary_search(array, value_to_find)
+if index
+  puts "Valor encontrado no índice #{index}."
+else
+  puts "valor não encontrado."
 end
