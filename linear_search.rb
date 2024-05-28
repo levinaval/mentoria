352d9e23265f2872d961f77c5650b611a82eac3b
Levi Santos <levisantoslunae@gmail.com>
2024-05-27 23:33:26 -0300

    Atualisando Linear Search.rb

---
+++ linear_search.rb
@@ -0,0 +1,19 @@
+def linear_search(array, value)
+  array.each_with_index do |element, index|
+    if element == value
+      return index  # Retorna o índice do elemento encontrado
+    end
+  end
+  return nil  # Retorna nil se o elemento não for encontrado
+end
+
+# Exemplo de uso
+array = [10, 23, 45, 70, 11, 15]
+value_to_find = 70
+
+index = linear_search(array, value_to_find)
+if index
+  puts "Valor encontrado no índice #{index}."
+else
+  puts "Valor não encontrado."
+end
