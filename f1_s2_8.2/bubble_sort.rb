3b3354bb1a093335b442a193ca79508f31901fe6
Levi Santos <levisantoslunae@gmail.com>
2024-05-30 23:27:05 -0300

    Desenvolvendo Bubble Sort.rb

--- f1_s2_8.2/bubble_sort.rb
+++ f1_s2_8.2/bubble_sort.rb
@@ -2,8 +2,15 @@ def bublle_sort(array)
 n = array.length
   loop do
     swapped = false
-    (n - 1).times do 
-
+    (n - 1).times do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped = true
+      end
     end
+
+    break unless swapped
   end
+
+  array
 end
