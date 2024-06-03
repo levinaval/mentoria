1a1499041185ab65e1957e50a56e308aed62ed94
Levi Santos <levisantoslunae@gmail.com>
2024-06-03 00:01:38 -0300

    Concluindo Counting Sort.rb

--- f1_s2_8.2/counting_sort.rb
+++ f1_s2_8.2/counting_sort.rb
@@ -3,4 +3,31 @@ def counting_sort(array)
 
   # Encontra o valor máximo na lista.
   max_value = array.max
+
+  # Inicializa o array de contagem.
+  count = Array.new(max_value + 1, 0)
+
+  # Conta a ocorrência de cada elemento na lista.
+  array.each do |element|
+    count[element] += 1
+  end
+
+  # Modifica o array de contagem para conter posições acumuladas.
+  (1..max_value).each do |i|
+    count[i] += count[i - 1]
+  end
+
+  # Constroi a lista ordenada.
+  sorted_array = Array.new(array.length)
+  array.reverse.each do |element|
+    sorted_array[count[element] - 1] = element
+    count[element] -= 1
+  end
+
+  sorted_array
 end
+
+# Exemplo de uso
+array = [4, 2, 1, 1, 8, 5, 1, 5]
+sorted_array = counting_sort(array)
+puts "Array ordenado: #{sorted_array}"
