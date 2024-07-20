692224fdd9da75e915c862e8b5ede1ee8596d251
Levi Santos <levisantosluna@gmail.com>
2024-07-20 12:18:19 -0300

    Maçâ

--- algoritmos/maca.rb
+++ algoritmos/maca.rb
@@ -2,7 +2,7 @@ puts "Você quer uma maçã? 1 - Sim 2 - Não"
 option = gets.chomp.to_f 
 
 if option == 1
-    puts "Levante"
+    puts "Levante e vá até a geladeira."
 elsif option == 2
     puts "ok"
 else
