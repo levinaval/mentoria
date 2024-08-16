872633218082e85f483785452ead7966ed4dca33
Levi Santos <levisantosluna@gmail.com>
2024-08-16 18:43:57 -0300

    Concluindo o Elsif

--- algoritmos/maca.rb
+++ algoritmos/maca.rb
@@ -18,8 +18,9 @@ if encontrou == 1
     puts "Pegue a maçã, vai querer cortar ou comer inteira? Cortar - 1 Inteira - 2"
     escolha = gets.chomp.to_f
     if escolha == 1
-        puts "Pegue a faca, corte a maçã e coloque no prato." 
-     elsif
+        puts "Lave a fruta. Pegue a faca, corte a maçã e coloque no prato. E um bom apetite!" 
+     elsif == 2
+        puts "Lave a fruta e tenha um bom apetite!"
     end
  elsif encontrou == 2
     puts "Você vai ter que ir ao mercado!"
