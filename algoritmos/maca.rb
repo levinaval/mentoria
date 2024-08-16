27d45be2d8165ebeebea6d3180fe294dfadb95b0
Levi Santos <levisantosluna@gmail.com>
2024-08-16 19:07:35 -0300

    Concluindo Maçã e incluindo um loop e um exit.

--- algoritmos/maca.rb
+++ algoritmos/maca.rb
@@ -1,29 +1,33 @@
-puts "Você quer uma maçã? 1 - Sim 2 - Não"
-option = gets.chomp.to_f 
+loop do
+    puts "Você quer uma maçã? 1 - Sim 2 - Não"
+    option = gets.chomp.to_f 
 
-if option == 1
-    puts "Levante e vá até a geladeira."
-elsif option == 2
-    puts "ok"
-else
-    puts "Opção inválida!"
+    if option == 1
+        puts "Levante e vá até a geladeira."
+    elsif option == 2
+        puts "Entendi. Você não quer uma maçã!"
+        exit
+    else
+        puts "Opção inválida!"
+    end
 end
 
-puts "Abra a geladeira e olhe onde está a maçã."
+loop do
+    puts "Abra a geladeira e olhe onde está a maçã."
+    puts "Encontrou? 1 - Sim 2 - Não" 
+    encontrou = gets.chomp.to_f
 
-puts "Encontrou? 1 - Sim 2 - Não" 
-encontrou = gets.chomp.to_f
-
-if encontrou == 1
-    puts "Pegue a maçã, vai querer cortar ou comer inteira? Cortar - 1 Inteira - 2"
-    escolha = gets.chomp.to_f
-    if escolha == 1
-        puts "Lave a fruta. Pegue a faca, corte a maçã e coloque no prato. E um bom apetite!" 
-     elsif == 2
-        puts "Lave a fruta e tenha um bom apetite!"
+    if encontrou == 1
+        puts "Pegue a maçã, vai querer cortar ou comer inteira? Cortar - 1 Inteira - 2"
+        escolha = gets.chomp.to_f
+        if escolha == 1
+            puts "Lave a fruta. Pegue a faca, corte a maçã e coloque no prato. E um bom apetite!" 
+        elsif escolha == 2
+            puts "Lave a fruta e tenha um bom apetite!"
+        end
+    elsif encontrou == 2
+        puts "Você vai ter que ir ao mercado!"
+    else
+        puts "Opção não válida!"
     end
- elsif encontrou == 2
-    puts "Você vai ter que ir ao mercado!"
- else
-    puts "Opção não válida!"
 end
