5af54de0648fb500618f04a3cc95630f3ecb0de5
Levi Santos <levisantosluna@gmail.com>
2024-08-20 04:23:35 -0300

    Finalizando Maçã

--- algoritmos/maca.rb
+++ algoritmos/maca.rb
@@ -1,59 +1,41 @@
-def prmpt(message)
+def prompt(message)
     puts message
     gets.chomp.to_f
 end
 
 def handle_maca
     puts "Levante e vá até a geladeira."
-    encontrou = prompt("Abra a geladeira e olhe onde está a maçã. Encontrou?")
+
+    encontrou = prompt("Abra a geladeira e olhe onde está a maçã. Encontrou? 1 - Sim 2 - Não")
 
     case encontrou
-        when 1
-            escolha = prompt("Pegue a maçã. Vai querer cortar ou comer inteira? 1 - Cortar 2 - Inteira")
-            if escolha == 1
-                puts "Lave a fruta. Pegue a faca, corte a maçã e cologue no prato. e tenha um bom apetite!"
-                elsif escolha == 2
-                    puts "Lave a fruta e tenha um bom apetite!"
-                else
-                    puts "Opção não valida!"
-            end
-        when 2
-            puts "Você vai ter que ir ao mercado."
-        else
-            puts "Opção não válida!"
+    when 1
+        escolha = prompt("Pegue a maçã. Vai querer cortar ou comer inteira? 1 - Cortar 2 - Inteira")
+        if escolha == 1
+            puts "Lave a fruta. Pegue a faca, corte a maçã e cologue no prato. e tenha um bom apetite!"
+            elsif escolha == 2
+                puts "Lave a fruta e tenha um bom apetite!"
+            else
+                puts "Opção não valida!"
+        end
+    when 2
+        puts "Você vai ter que ir ao mercado."
+    else
+        puts "Opção não válida!"
     end
 end
 
-#loop do
-#    puts "Você quer uma maçã? 1 - Sim 2 - Não"
-#    option = gets.chomp.to_f 
-#
-#    if option == 1
-#        puts "Levante e vá até a geladeira."
-#    elsif option == 2
-#        puts "Entendi. Você não quer uma maçã!"
-#        exit
-#    else
-#        puts "Opção inválida!"
-#    end
-#end
+loop do
+    option = prompt("Você quer uma maçã? 1 - Sim 2 - Não")
 
-#loop do
-#    puts "Abra a geladeira e olhe onde está a maçã."
-#    puts "Encontrou? 1 - Sim 2 - Não" 
-#    encontrou = gets.chomp.to_f
-#
-#    if encontrou == 1
-#        puts "Pegue a maçã, vai querer cortar ou comer inteira? Cortar - 1 Inteira - 2"
-#        escolha = gets.chomp.to_f
-#        if escolha == 1
-#            puts "Lave a fruta. Pegue a faca, corte a maçã e coloque no prato. E um bom apetite!" 
-#        elsif escolha == 2
-#            puts "Lave a fruta e tenha um bom apetite!"
-#        end
-#    elsif encontrou == 2
-#        puts "Você vai ter que ir ao mercado!"
-#    else
-#        puts "Opção não válida!"
-#    end
-#end
+    case option
+    when 1
+      handle_maca
+      break
+    when 2
+        puts "Entendi. Você não quer uma maçã!"
+        break
+    else
+        puts "Opção inválida!"        
+    end
+end
