af0cf348fdec5d16bb4aa661654208119d3aa3d4
Levi Santos <levisantosluna@gmail.com>
2024-08-25 23:41:07 -0300

    Criando Teste.rb

---
+++ algoritmos/teste.rb
@@ -0,0 +1,42 @@
+def prompt(message)
+    puts message
+    gets.chomp.to_f
+  end
+  
+  def handle_maca
+    puts "Levante e vá até a geladeira."
+  
+    encontrou = prompt("Abra a geladeira e olhe onde está a maçã. Encontrou? 1 - Sim 2 - Não")
+  
+    case encontrou
+    when 1
+      escolha = prompt("Pegue a maçã. Vai querer cortar ou comer inteira? 1 - Cortar 2 - Inteira")
+      if escolha == 1
+        puts "Lave a fruta. Pegue a faca, corte a maçã e coloque no prato. Tenha um bom apetite!"
+      elsif escolha == 2
+        puts "Lave a fruta e tenha um bom apetite!"
+      else
+        puts "Opção não válida!"
+      end
+    when 2
+      puts "Você vai ter que ir ao mercado."
+    else
+      puts "Opção não válida!"
+    end
+  end
+  
+  loop do
+    option = prompt("Você quer uma maçã? 1 - Sim 2 - Não")
+  
+    case option
+    when 1
+      handle_maca
+      break
+    when 2
+      puts "Entendi. Você não quer uma maçã!"
+      break
+    else
+      puts "Opção inválida!"
+    end
+  end
+  
